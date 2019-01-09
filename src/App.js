fa8c28714665bfb608a450ea347c404ba9ebfd68
Oluwatosin Awofeso <midastouchproductions@gmail.com>
2019-01-08 20:47:38 -0600

    number guesser : :

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rightNumber: 5,
+      correct: false
+    };
+  }
+  guessNumber(num) {
+    //if this.state.rightNumber is the same as num then set this.state.correct to true otherwise its false
+
+    this.setState({ correct: this.state.rightNumber === num });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.correct && (
+          <div className="right">
+            <p>Right Answer</p>
+          </div>
+        )}
+        {!this.state.correct && (
+          <div className="wrong">
+            <p>Wrong Answer</p>
+          </div>
+        )}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num, i) => {
+          return (
+            <div
+              key={i}
+              onClick={() => this.guessNumber(num)}
+              className="num-box"
+            >
+              <p>{num}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
